d8bf9be2c88708cc5a1d623e58a2dd17e2ae043a
DICREY <dicreyraptor@gmail.com>
2025-08-07 19:17:37 -0500

    Second Commit

---
+++ FrontEnd/src/app/Component/navbar/navbar.component.ts
@@ -0,0 +1,18 @@
+// Librarys
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+// Decors 
+@Component({
+  selector: 'app-navbar',
+  imports: [
+    RouterLink, RouterLinkActive
+  ],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+
+// class 
+export class NavbarComponent {
+
+}
